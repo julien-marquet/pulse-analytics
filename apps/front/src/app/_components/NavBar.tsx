45d2cf5ae1605b1408bc6c15eed049ad2c3c1da5
Julien Marquet <julienmarquet9+gitlab@gmail.com>
2026-04-10 15:17:35 +0200

    basic nextjs app

---
+++ apps/front/src/app/_components/NavBar.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { ReactNode } from "react";
+
+// type NavBarProps = {}
+
+export default function NavBar() {
+
+    const pathName = usePathname();
+    console.log(pathName);
+    return (
+        <nav>
+            <NavBarItem pathName="/overview" activePathName={pathName} >Overview</NavBarItem>
+            <NavBarItem pathName="/events" activePathName={pathName} >Events</NavBarItem>
+            <NavBarItem pathName="/stats" activePathName={pathName} >Stats</NavBarItem>
+        </nav>
+    )
+}
+
+
+type NavBarItemProps = {
+    activePathName: string;
+    pathName: string;
+    children: ReactNode;
+}
+function NavBarItem({ children, activePathName, pathName }: NavBarItemProps) {
+    const isActive = activePathName == pathName;
+
+    return <li>
+        <Link className={isActive ? "bg-black" : "bg-white"} href={pathName}>
+            {children}
+        </Link>
+    </li>
+
+}
