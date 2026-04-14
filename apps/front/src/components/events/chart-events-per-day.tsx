b1c1470d0db318ebd5278258c6293bcd8720fcac
Julien Marquet <julienmarquet9+gitlab@gmail.com>
2026-04-14 14:59:22 +0200

    nicer colors

--- apps/front/src/components/events/chart-events-per-day.tsx
+++ apps/front/src/components/events/chart-events-per-day.tsx
@@ -77,10 +77,14 @@ export default function ChartEventsPerDay({
               <linearGradient id="eventsGradient" x1="0" y1="0" x2="0" y2="1">
                 <stop
                   offset="5%"
-                  stopColor="var(--chart-2)"
+                  stopColor="var(--chart-5)"
                   stopOpacity={0.8}
                 />
-                <stop offset="95%" stopColor="var(--chart-2)" stopOpacity={0} />
+                <stop
+                  offset="95%"
+                  stopColor="var(--chart-3)"
+                  stopOpacity={0.1}
+                />
               </linearGradient>
             </defs>
             <CartesianGrid vertical={false} />
